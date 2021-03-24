3336d52fa32a6535ad280c26099cc4a7ad8bc617
Aleksander Tjernes <aleksander.tjernes@gmail.com>
2021-03-24 11:30:42 +0100

    feat: add tasks and tasks configs

--- python_logger.py
+++ python_logger.py
@@ -32,9 +32,51 @@ dag = DAG(
         start_date=datetime.datetime.now() - datetime.timedelta(days=30),
         schedule_interval='@monthly')
 
-# add a greet task for Airflow to execute
-greet_task = PythonOperator(
-    task_id="greet_task", # id could by anything as long as it is somewhat descriptive
-    python_callable=say_hi, # passing function to PythonOperator so it knows which function to execute
-    dag=dag # assign 'dag' to the DAG field for the PythonOperator so it knows what DAG it belongs to
+# add tasks for Airflow to execute
+
+# Parameters:
+# task_id = name it anything as long as it is somewhat descriptive
+# python_callable = passing function to PythonOperator so it knows which function to execute
+# dag = assign 'dag' to the DAG field for the PythonOperator so it knows what DAG it belongs to
+
+
+# Define an say hi task that calls the `say_hi` function above
+say_hi_task = PythonOperator(
+    task_id="say_hi",
+    python_callable=say_hi,
+    dag=dag
 )
+
+# Define an addition task that calls the `addition` function above
+addition_task = PythonOperator(
+    task_id="addition_task",
+    python_callable=addition,
+    dag=dag
+)
+
+# Define a subtraction task that calls the `subtraction` function above
+subtraction_task = PythonOperator(
+    task_id="subtraction_task",
+    python_callable=subtraction,
+    dag=dag
+)
+
+# Define a division task that calls the `division` function above
+division_task = PythonOperator(
+    task_id="division_task",
+    python_callable=division,
+    dag=dag
+)
+
+# Configuring task dependencies such that the graph looks like the following:
+#
+#                    -> addition_task
+#                   /                  \
+#        say_hi_task                    -> division_task
+#                   \                  /
+#                    ->subtraction_task
+
+say_hi_task >> addition_task
+say_hi_task >> subtraction_task
+division_task << addition_task
+division_task << subtraction_task
