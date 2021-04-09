e66dc84b7b0a5ee197ed5f8dd15a6e325d873e01
Aleksander Tjernes <aleksander.tjernes@gmail.com>
2021-04-09 09:31:20 +0200

    feat: add submitted udacity project

---
+++ plugins/operators/load_dimension.py
@@ -0,0 +1,34 @@
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadDimensionOperator(BaseOperator):
+    ui_color = '#80BD9E'
+
+    @apply_defaults
+    def __init__(self,
+                 # Defining operators params
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 table="",
+                 sql="",
+                 *args, **kwargs):
+
+        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+        # Map params
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.aws_credentials_id = aws_credentials_id
+        self.sql = sql
+
+    def execute(self, context):
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info("Clearing data from destination Redshift table")
+        redshift.run("DELETE FROM {}".format(self.table))
+        
+        self.log.info("Inserting data into {}".format(self.table))
+        redshift.run(self.sql)
